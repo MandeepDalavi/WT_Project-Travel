07c7e99002f7b98b78d8a0cdead7dee4ff7e5b9d
MandeepDalavi <mandeepdalavi@gmail.com>
2022-06-04 14:07:16 +0530

    booking

--- book.php
+++ book.php
@@ -29,6 +29,57 @@
 
 </section>
 
+<div class="heading" style="background:url(images/header-bg-3.png) no-repeat">
+   <h1>book now</h1>
+</div>
+
+<section class="booking">
+
+   <h1 class="heading-title">book your trip!</h1>
+
+   <form action="book_form.php" method="post" class="book-form">
+
+      <div class="flex">
+         <div class="inputBox">
+            <span>name :</span>
+            <input type="text" placeholder="enter your name" name="name" required>
+         </div>
+         <div class="inputBox">
+            <span>email :</span>
+            <input type="email" placeholder="enter your email" name="email" required>
+         </div>
+         <div class="inputBox">
+            <span>phone :</span>
+            <input type="number" placeholder="enter your number" name="phone" required>
+         </div>
+         <div class="inputBox">
+            <span>address :</span>
+            <input type="text" placeholder="enter your address" name="address" required>
+         </div>
+         <div class="inputBox">
+            <span>where to :</span>
+            <input type="text" placeholder="place you want to visit" name="location" required>
+         </div>
+         <div class="inputBox">
+            <span>how many :</span>
+            <input type="number" placeholder="number of guests" name="guests" required>
+         </div>
+         <div class="inputBox">
+            <span>arrivals :</span>
+            <input type="date" id="arrivalDate" onChange="changeDate();" name="arrivals" required>
+         </div>
+         <div class="inputBox">
+            <span>leaving :</span>
+            <input type="date" id="leavingDate" name="leaving" required>
+         </div>
+      </div>
+      <input type="hidden" id="current_date" name="currentDate">
+      <input type="submit" value="submit" class="btn" name="send">
+
+   </form>
+
+</section>
+
 <section class="footer">
 
    <div class="box-container">
@@ -70,5 +121,56 @@
    <div class="credit"> created by <span>Mandeep Dalavi</span> | all rights reserved! </div>
 
 </section>
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+<script>
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth();
+    var day = date.getDate();
+    var weekDay = date.getDay();
+    var monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var weekDayArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    month = monthArr[month];
+    weekDay = weekDayArr[weekDay];
+    document.getElementById("current_date").value = weekDay + ", " + month + " " + day + ", " + year;
+
+    var dd = date.getDate() + 1;
+    var mm = date.getMonth() + 1; //January is 0!
+    var yyyy = date.getFullYear();
+    if(dd<10){
+      dd='0'+dd
+    } 
+    if(mm<10){
+      mm='0'+mm
+    }
+    
+    var tomorrow = yyyy+'-'+mm+'-'+dd;
+    document.getElementById("arrivalDate").setAttribute("min", tomorrow);
+    yyyy++;
+    var endDate = yyyy+'-'+'01'+'-'+31;
+    document.getElementById("arrivalDate").setAttribute("max", endDate);
+
+    var arrival = document.getElementById("arrivalDate");
+    var leaving = document.getElementById("leavingDate");
+    function changeDate() {
+      var arrivalD = new Date(arrival.value);
+      var leaveDD = arrivalD.getDate() + 1;
+      var leaveMM = arrivalD.getMonth() + 1; //January is 0!
+      var leaveYYYY = arrivalD.getFullYear();
+      if(leaveDD<10){
+        leaveDD='0'+leaveDD
+      }
+      if(leaveMM<10){
+        leaveMM='0'+leaveMM
+      }
+      
+      var minleaveDate = leaveYYYY+'-'+leaveMM+'-'+leaveDD;
+      leaving.setAttribute("min", minleaveDate);
+    }
+   
+</script>
 </body>
 </html>
